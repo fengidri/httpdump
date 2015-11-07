09100320f5da3f59acadcec4ef006fbec6c60375
fengidri <fengidri@gmail.com>
2015-11-07 14:24:37 +0000

    complete the parse for http

--- pcapparser/httpparser.py
+++ pcapparser/httpparser.py
@@ -8,7 +8,7 @@ from pcapparser.reader import DataReader
 from pcapparser import config
 
 from pcapparser.config import OutputLevel
-from io import StringIO
+from StringIO import StringIO
 
 class Stream(StringIO):
     def readline(self, length=None):
@@ -41,53 +41,19 @@ class Stream(StringIO):
         return r
 
 
-__author__ = 'dongliu'
-
 class HttpStream(object):
     def __init__(self):
         self.handle = None
+        self.headers = {}
+        self.body = Stream()
+        self._chunked = False
+        self._chunked_len = -2# this is special
+        self._len = -1
 
     def read(self, msg):
         return self.handle(msg)
 
-class HttpRequest(HttpStream):
-    def __init__(self):
-        self.method = b''
-        self.host = b''
-        self.uri = b''
-        self.reqline = {}
-        self.headers = {}
-        self.transfer_encoding = b''
-        self.content_encoding = b''
-        self.content_type = b''
-        self.compress = Compress.IDENTITY
-        self.expect = b''
-        self.protocol = b''
-
-
-        self.handle = self.reqline_handle
-
-        self._chunked = False
-        self._len = -1
-
-        self.raw_headers = []
-        self.
-
-    def reqline_handle(self, msg):
-        line = msg.readline()
-        if not line:
-            return
-
-        items = line.split(' ')
-        if len(items) < 2:
-            return -1 # NOT HTTP
-
-        self.raw_headers.append(line)
-        self.reqline['method'] = items[0]
-        self.reqline['uri'] = items[1]
-        self.handle = self.request_handle
-
-    def request_handle(self, msg):
+    def headers_handle(self, msg):
         while True:
             line = msg.readline()
             if None == line:
@@ -102,11 +68,79 @@ class HttpRequest(HttpStream):
             key, value = utils.parse_http_header(line)
             if key == None:
                 continue
+
             if key == 'content-length':
                 self._len = int(value)
+
+            elif key == 'transfer-encoding':
+                if value.find("chunked") > -1:
+                    self._chunked = True
+
+    def body_handle(self, msg):
+        if self._chunked:
+            return self.read_chunked_body(msg)
+
+        if self._len != -1:
+            t = msg.read(self._len)
+            self.body.write(t)
+            self._len -= len(t)
+
+        if 0 == self._len:
+            return 0 # OVER
+
+
+    def read_chunked_body(self, msg):
+        """ read chunked body """
+        if self._chunked_len > 0:
+            t = msg.read(self._chunked_len)
+            self.body.write(t)
+            self._chunked_len -= len(t)
+
+        if self._chunked_len <= 0 and self._chunked_len > -2:
+            t = msg.read(2 + self._chunked_len)
+            self._chunked_len -= len(t)
+
+        if self._chunked_len == -2:
+            line = msg.readline()
+            chunk_size_end = cline.find(b';')
+            if chunk_size_end < 0:
+                chunk_size_end = len(cline)
+                # skip chunk extension
+            chunk_size_str = cline[0:chunk_size_end]
+            if chunk_size_str[0] == b'0':
+                return 0 # over
+
+            try:
+                chunk_len = int(chunk_size_str, 16)
+                self._chunked_len = chunk_len
+                return self.read_chunked_body(msg)
+            except:
+                pass
+
+
+class HttpRequest(HttpStream):
+    def __init__(self):
+        self.reqline = {}
+        self.raw_headers = []
+
+        self.handle = self.reqline_handle
+
+
+    def reqline_handle(self, msg):
+        line = msg.readline()
+        if not line:
+            return
+
+        items = line.split(' ')
+        if len(items) < 2:
+            return -1 # NOT HTTP
+
+        self.raw_headers.append(line)
+        self.reqline['method'] = items[0]
+        self.reqline['uri'] = items[1]
+        self.handle = self.headers_handle
+
         # handle the headrs over
-
-
 
 
     def URI(self):
@@ -118,211 +152,21 @@ class HttpRequest(HttpStream):
 
 class HttpResponse(HttpStream):
     def __init__(self):
+        self.resline = {}
         self.content_len = 0
-        self.status_line = None
-        self.status_code = None
-        self.transfer_encoding = b''
-        self.content_encoding = b''
-        self.content_type = b''
-        self.compress = Compress.IDENTITY
-        self.chunked = False
-        self.connection_close = False
-        self.raw_data = None
 
-
-class httpparser(object):
-    def read_headers(self, reader, lines):
-        """
-        :type reader: DataReader
-        :type lines: list
-        :return: dict
-        """
-        header_dict = defaultdict(str)
-        while True:
-            line = reader.read_line()
-            if line is None:
-                break
-            line = line.strip()
-            if not line:
-                break
-            lines.append(line)
-
-            key, value = utils.parse_http_header(line)
-            if key is None:
-                # incorrect headers.
-                continue
-
-            header_dict[key.lower()] = value
-        return header_dict
-
-    def read_http_req_header(self, reader):
-        """read & parse http headers"""
-        line = reader.read_line()
-        if line is None:
-            return None
-        line = line.strip()
-        if not utils.is_request(line):
-            return None
-
-        req_header = HttpRequestHeader()
-        items = line.split(b' ')
-        if len(items) == 3:
-            req_header.method = items[0]
-            req_header.uri = items[1]
-            req_header.protocol = items[2]
-
-        lines = [line]
-        header_dict = self.read_headers(reader, lines)
-        if b"content-length" in header_dict:
-            req_header.content_len = int(header_dict[b"content-length"])
-        if b"transfer-encoding" in header_dict and b'chunked' in header_dict[b"transfer-encoding"]:
-            req_header.chunked = True
-        req_header.content_type = header_dict[b'content-type']
-        req_header.compress = utils.get_compress_type(header_dict[b"content-encoding"])
-        req_header.host = header_dict[b"host"]
-        if b'expect' in header_dict:
-            req_header.expect = header_dict[b'expect']
-
-        req_header.raw_data = b'\n'.join(lines)
-        return req_header
-
-    def read_http_resp_header(self, reader):
-        """read & parse http headers"""
-        line = reader.read_line()
-        if line is None:
-            return line
-        line = line.strip()
-
-        if not utils.is_response(line):
-            return None
-        resp_header = HttpResponseHeader()
-        resp_header.status_line = line
-        try:
-            resp_header.status_code = int(line.split(' ')[1])
-        except:
-            pass
-
-        lines = [line]
-        header_dict = self.read_headers(reader, lines)
-        if b"content-length" in header_dict:
-            resp_header.content_len = int(header_dict[b"content-length"])
-        if b"transfer-encoding" in header_dict and b'chunked' in header_dict[b"transfer-encoding"]:
-            resp_header.chunked = True
-        resp_header.content_type = header_dict[b'content-type']
-        resp_header.compress == utils.get_compress_type(header_dict[b"content-encoding"])
-        resp_header.connection_close = (header_dict[b'connection'] == b'close')
-        resp_header.raw_data = b'\n'.join(lines)
-        return resp_header
-
-    def read_chunked_body(self, reader, skip=False):
-        """ read chunked body """
-        result = []
-        # read a chunk per loop
-        while True:
-            # read chunk size line
-            cline = reader.read_line()
-            if cline is None:
-                # error occurred.
-                if not skip:
-                    return b''.join(result)
-                else:
-                    return
-            chunk_size_end = cline.find(b';')
-            if chunk_size_end < 0:
-                chunk_size_end = len(cline)
-                # skip chunk extension
-            chunk_size_str = cline[0:chunk_size_end]
-            # the last chunk
-            if chunk_size_str[0] == b'0':
-                # chunk footer header
-                # TODO: handle additional http headers.
-                while True:
-                    cline = reader.read_line()
-                    if cline is None or len(cline.strip()) == 0:
-                        break
-                if not skip:
-                    return b''.join(result)
-                else:
-                    return
-                    # chunk size
-            chunk_size_str = chunk_size_str.strip()
-            try:
-                chunk_len = int(chunk_size_str, 16)
-            except:
-                return b''.join(result)
-
-            data = reader.read(chunk_len)
-            if data is None:
-                # skip all
-                # error occurred.
-                if not skip:
-                    return b''.join(result)
-                else:
-                    return
-            if not skip:
-                result.append(data)
-
-            # a CR-LF to end this chunked response
-            reader.read_line()
-
-    def read_request(self, reader, message):
-        """ read and output one http request. """
-        if message.expect_header and not utils.is_request(reader.fetch_line()):
-            req_header = message.expect_header
-            message.expect_header = None
-        else:
-            req_header = self.read_http_req_header(reader)
-            if req_header is None:
-                # read header error, we skip all data.
-                reader.skip_all()
-                return
-            if req_header.expect:
-                # it is expect:continue-100 post request
-                message.expect_header = req_header
-
-        # deal with body
-        if not req_header.chunked:
-            content = reader.read(req_header.content_len)
-        else:
-            content = self.read_chunked_body(reader)
-
-        _filter = config.get_filter()
-        show = _filter.by_domain(req_header.host) and _filter.by_uri(req_header.uri)
-        message.filtered = not show
-        if show:
-            self.msgs.append((0, req_header, content))
-
-    def read_response(self, reader, message):
-        """
-        read and output one http response
-        """
-        resp_header = self.read_http_resp_header(reader)
-        if resp_header is None:
-            reader.skip_all()
+    def reqline_handle(self, msg):
+        line = msg.readline()
+        if not line:
             return
 
-        if message.expect_header:
-            if resp_header.status_code == 100:
-                # expected 100, we do not read body
-                reader.skip_all()
-                return
+        items = line.split(' ')
+        if len(items) < 2:
+            return -1 # NOT HTTP
 
-        # read body
-        if not resp_header.chunked:
-            if resp_header.content_len == 0:
-                if resp_header.connection_close:
-                    # we can't get content length, so assume it till the end of data.
-                    resp_header.content_len = 10000000
-                else:
-                    # we can't get content length, and is not a chunked body, we cannot do nothing,
-                    # just read all data.
-                    resp_header.content_len = 10000000
-            content = reader.read(resp_header.content_len)
-        else:
-            content = self.read_chunked_body(reader)
-
-        if not message.filtered:
-            self.msgs.append((1, resp_header, content))
+        self.raw_headers.append(line)
+        self.reqline['status'] = items[1]
+        self.handle = self.headers_handle
 
 
 class HttpParser(httpparser):
@@ -336,7 +180,7 @@ class HttpParser(httpparser):
         self.tcp = tcp
         self.errno = 0
 
-        self.stream =  [StringIO(), StringIO()]
+        self.stream =  [String(), String()]
         self.handles = [None, None]
 
         self.msgs = []
@@ -370,7 +214,10 @@ class HttpParser(httpparser):
             self.handles[http_type] = rr
 
         stream.seek(0)
-        return rr.read(stream)
+        res = rr.read(stream)
+        if 0 == res:
+            self.handles[http_type] = None
+        return res
 
     def print(self, level):
         if not self.msgs:
